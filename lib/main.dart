fc65aa051e975cd91d1e60de24d4ccdd78ba564a
nixen2802 <n.mandliya@somaiya.edu>
2021-10-19 17:17:31 +0530

    Another page added

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'StudentZone'),
     );
   }
 }
@@ -40,17 +40,30 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         title: Text(widget.title),
         backgroundColor: Colors.red,
       ),
-      // body: new Container(
+      // body: ListView( 
+      //   shrinkWrap: true,
+      // children: [
+      //   new Container(
       //   padding: new EdgeInsets.all(32.0),
       //   child: new Center(
       //     child: new Column(
       //       children: [
-      //         new Text('REGISTER', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-      //         Padding(padding: EdgeInsets.all(10.0)),
+      //         Align(
+      //           alignment: Alignment.centerLeft,
+      //           child: Container(
+      //             color: Colors.white,
+      //             child: Text(
+      //               'REGISTER',
+      //               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)
+      //            ),
+      //           ),
+      //         ),
+      //         Padding(padding: EdgeInsets.all(20.0)),
       //         new TextField(
       //           decoration: InputDecoration(
       //             hintText: 'Name',
@@ -96,30 +109,36 @@ class _MyHomePageState extends State<MyHomePage> {
       //           ),
       //         ),
       //         Padding(padding: EdgeInsets.all(10.0)),
-      //         new ElevatedButton(onPressed: null,
-      //         child: Center(
-      //           child: Text('REGISTER', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25, color: Colors.red)),
-      //         ),
-      //         style: ElevatedButton.styleFrom(
-      //           fixedSize: Size(314,60),
-      //           primary: Colors.red,
-      //           shape: RoundedRectangleBorder(
-      //             borderRadius: BorderRadius.circular(20)
-      //           )
-      //           ),
-      //         )
+      //         new RaisedButton(
+      //           onPressed: (){print("Null");}, 
+      //           child: Text('REGISTER'), 
+      //           color: Colors.red,
+      //           textColor: Colors.black,
+      //           shape: const RoundedRectangleBorder(
+      //             side: const BorderSide(color: Colors.black, width: 2),
+      //             ),
+      //             padding: EdgeInsets.fromLTRB(125, 20, 130, 20),),
       //       ],
       //     )
       //   )
+      // )]
       // )
 
       // body: new Container(
       //   padding: new EdgeInsets.all(32.0),
-      //   child: new Center(
       //     child: new Column(
       //       children: [
-      //         Padding(padding: EdgeInsets.all(50.0)),
-      //         new Text('LOGIN', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
+      //         Padding(padding: EdgeInsets.fromLTRB(0, 100, 0, 0)),
+      //         Align(
+      //           alignment: Alignment.centerLeft,
+      //           child: Container(
+      //             color: Colors.white,
+      //             child: Text(
+      //               'LOGIN',
+      //               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)
+      //            ),
+      //           ),
+      //         ),
       //         Padding(padding: EdgeInsets.all(10.0)),
       //         new TextField(
       //           decoration: InputDecoration(
@@ -131,7 +150,7 @@ class _MyHomePageState extends State<MyHomePage> {
       //         ),
       //         Padding(padding: EdgeInsets.all(10.0)),
       //         new TextField(
-      //           decoration: InputDecoration(
+      //           decoration: const InputDecoration(
       //             hintText: 'Password',
       //             enabledBorder: OutlineInputBorder(
       //               borderSide: BorderSide(color: Colors.black, width: 2.0)
@@ -139,51 +158,128 @@ class _MyHomePageState extends State<MyHomePage> {
       //           ),
       //         ),
       //         Padding(padding: EdgeInsets.all(10.0)),
-      //         new ElevatedButton(onPressed: null,
-      //         child: Center(
-      //           child: Text('LOGIN', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25, color: Colors.red)),
-      //         ),
-      //         style: ElevatedButton.styleFrom(
-      //           fixedSize: Size(314,60),
-      //           primary: Colors.red,
-      //           shape: RoundedRectangleBorder(
-      //             borderRadius: BorderRadius.circular(20)
-      //           )
-      //           ),
-      //         )
+      //         new RaisedButton(
+      //           onPressed: (){print("Null");}, 
+      //           child: Text('LOG IN'), 
+      //           color: Colors.red,
+      //           textColor: Colors.black,
+      //           shape: const RoundedRectangleBorder(
+      //             side: const BorderSide(color: Colors.black, width: 2),
+      //             ),
+      //             padding: EdgeInsets.fromLTRB(132, 20, 130, 20),),
       //       ],
       //     )
-      //   )
       // )
+
+
+      // body: Center(
+      //   child: Column(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: <Widget>[
+      //       Padding(padding: EdgeInsets.all(160.0)),
+      //       Center(
+      //         child:
+      //         Text(
+      //         "StudentZone",
+      //         style: TextStyle(
+      //           color: Colors.red,
+      //           fontSize: 30
+      //         )
+      //       ),
+      //       ),
+      //       Padding(padding: EdgeInsets.all(140.0)),
+      //       Center(
+      //         child: Row(
+      //           mainAxisAlignment: MainAxisAlignment.center,
+      //           children: [
+      //         //     new ElevatedButton(onPressed: null, child: Text('LOGIN'), 
+      //         //     style: ButtonStyle(
+      //         //     backgroundColor: MaterialStateProperty.all(Colors.white),
+      //         //     textStyle: MaterialStateProperty.all(TextStyle(fontSize: 25)),
+      //         //   ),
+      //         // ),
+      //         new RaisedButton(
+      //           onPressed: (){print("Null");}, 
+      //           child: Text('LOG IN'), 
+      //           color: Colors.white,
+      //           textColor: Colors.black,
+      //           shape: RoundedRectangleBorder(
+      //             side: BorderSide(color: Colors.black, width: 2)
+      //             ),
+      //           padding: EdgeInsets.fromLTRB(50, 25, 50, 25),
+      //           ),
+      //           Padding(padding: EdgeInsets.fromLTRB(20, 0, 0, 0)),
+      //           new RaisedButton(
+      //           onPressed: (){print("Null");}, 
+      //           child: Text('REGISTER'), 
+      //           color: Colors.red,
+      //           textColor: Colors.white,
+      //           shape: RoundedRectangleBorder(
+      //             side: BorderSide(color: Colors.red, width: 2)
+      //             ),
+      //           padding: EdgeInsets.fromLTRB(50, 25, 50, 25),
+      //           ),
+      //         // new ElevatedButton(onPressed: null, child: Text('REGISTER'), style: ButtonStyle(
+      //         // backgroundColor: MaterialStateProperty.all(Colors.red),
+      //         // textStyle: MaterialStateProperty.all(TextStyle(fontSize: 25))
+      //         // ))
+      //         ],
+      //       ),
+      //       ),
+      //     ],
+      //   ),
+      // ),
 
 
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "StudentZone",
-              style: TextStyle(
-                color: Colors.red,
-                fontSize: 30
-              )
+          children: [
+            Padding(padding: EdgeInsets.fromLTRB(100, 50, 100, 0)),
+            CircleAvatar(
+            radius: 70,
+            backgroundImage: NetworkImage('https://previews.123rf.com/images/jemastock/jemastock1706/jemastock170608711/80128439-young-and-successful-business-man-cartoon-employee-work.jpg')
             ),
-            Expanded(
-              child: Align(
-                alignment: FractionalOffset.bottomCenter,
-                child: ButtonBar(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-              new ElevatedButton(onPressed: null, child: Text('LOGIN'), style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.white),
-                textStyle: MaterialStateProperty.all(TextStyle(fontSize: 25))
-              )),
-              new ElevatedButton(onPressed: null, child: Text('REGISTER'), style: ButtonStyle(
-              backgroundColor: MaterialStateProperty.all(Colors.red),
-              textStyle: MaterialStateProperty.all(TextStyle(fontSize: 25))
-              ))
-            ])
-              )
+            Text(
+                    'Nayan Mandliya',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)
+            ),
+            Padding(padding: EdgeInsets.fromLTRB(100, 20, 100, 0)),
+            Text(
+                    'nayan@gmail.com',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15)
+            ),
+            Padding(padding: EdgeInsets.fromLTRB(100, 20, 100, 0)),
+            new RaisedButton(
+                onPressed: (){print("Null");}, 
+                child: Text('EDIT PROFILE'), 
+                color: Colors.red,
+                textColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(color: Colors.red, width: 2)
+                  ),
+                padding: EdgeInsets.fromLTRB(100, 25, 100, 25),
+                ),
+                Padding(padding: EdgeInsets.fromLTRB(100, 20, 100, 0)),
+                new RaisedButton(
+                onPressed: (){print("Null");}, 
+                child: Text('ADDS'), 
+                color: Colors.white,
+                textColor: Colors.black,
+                shape: RoundedRectangleBorder(
+                  side: BorderSide(color: Colors.black, width: 2)
+                  ),
+                padding: EdgeInsets.fromLTRB(125, 20, 120, 20),
+                ),
+                Padding(padding: EdgeInsets.fromLTRB(100, 20, 100, 0)),
+                Container(
+                height: 150.0,
+                width: 400.0,
+                color: Colors.transparent,
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.all(Radius.circular(5.0))),
+              ),
             ),
           ],
         ),
