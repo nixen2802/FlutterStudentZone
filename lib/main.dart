aa3ed08548d6faee7cb4df68067ac8471adc62c2
nixen2802 <n.mandliya@somaiya.edu>
2021-10-27 18:01:27 +0530

    Temp commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'signin.dart';
 
 void main() {
   runApp(MyApp());
@@ -231,59 +232,60 @@ class _MyHomePageState extends State<MyHomePage> {
       // ),
 
 
-      body: Center(
-        child: Column(
-          children: [
-            Padding(padding: EdgeInsets.fromLTRB(100, 50, 100, 0)),
-            CircleAvatar(
-            radius: 70,
-            backgroundImage: NetworkImage('https://previews.123rf.com/images/jemastock/jemastock1706/jemastock170608711/80128439-young-and-successful-business-man-cartoon-employee-work.jpg')
-            ),
-            Text(
-                    'Nayan Mandliya',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)
-            ),
-            Padding(padding: EdgeInsets.fromLTRB(100, 20, 100, 0)),
-            Text(
-                    'nayan@gmail.com',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15)
-            ),
-            Padding(padding: EdgeInsets.fromLTRB(100, 20, 100, 0)),
-            new RaisedButton(
-                onPressed: (){print("Null");}, 
-                child: Text('EDIT PROFILE'), 
-                color: Colors.red,
-                textColor: Colors.white,
-                shape: RoundedRectangleBorder(
-                  side: BorderSide(color: Colors.red, width: 2)
-                  ),
-                padding: EdgeInsets.fromLTRB(100, 25, 100, 25),
-                ),
-                Padding(padding: EdgeInsets.fromLTRB(100, 20, 100, 0)),
-                new RaisedButton(
-                onPressed: (){print("Null");}, 
-                child: Text('ADDS'), 
-                color: Colors.white,
-                textColor: Colors.black,
-                shape: RoundedRectangleBorder(
-                  side: BorderSide(color: Colors.black, width: 2)
-                  ),
-                padding: EdgeInsets.fromLTRB(125, 20, 120, 20),
-                ),
-                Padding(padding: EdgeInsets.fromLTRB(100, 20, 100, 0)),
-                Container(
-                height: 150.0,
-                width: 400.0,
-                color: Colors.transparent,
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.grey,
-                      borderRadius: BorderRadius.all(Radius.circular(5.0))),
-              ),
-            ),
-          ],
-        ),
-      ),
+      body: Signin()
+      // Center(
+      //   child: Column(
+      //     children: [
+      //       Padding(padding: EdgeInsets.fromLTRB(100, 50, 100, 0)),
+      //       CircleAvatar(
+      //       radius: 70,
+      //       backgroundImage: NetworkImage('https://previews.123rf.com/images/jemastock/jemastock1706/jemastock170608711/80128439-young-and-successful-business-man-cartoon-employee-work.jpg')
+      //       ),
+      //       Text(
+      //               'Nayan Mandliya',
+      //               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)
+      //       ),
+      //       Padding(padding: EdgeInsets.fromLTRB(100, 20, 100, 0)),
+      //       Text(
+      //               'nayan@gmail.com',
+      //               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15)
+      //       ),
+      //       Padding(padding: EdgeInsets.fromLTRB(100, 20, 100, 0)),
+      //       new RaisedButton(
+      //           onPressed: (){print("Null");}, 
+      //           child: Text('EDIT PROFILE'), 
+      //           color: Colors.red,
+      //           textColor: Colors.white,
+      //           shape: RoundedRectangleBorder(
+      //             side: BorderSide(color: Colors.red, width: 2)
+      //             ),
+      //           padding: EdgeInsets.fromLTRB(100, 25, 100, 25),
+      //           ),
+      //           Padding(padding: EdgeInsets.fromLTRB(100, 20, 100, 0)),
+      //           new RaisedButton(
+      //           onPressed: (){print("Null");}, 
+      //           child: Text('ADDS'), 
+      //           color: Colors.white,
+      //           textColor: Colors.black,
+      //           shape: RoundedRectangleBorder(
+      //             side: BorderSide(color: Colors.black, width: 2)
+      //             ),
+      //           padding: EdgeInsets.fromLTRB(125, 20, 120, 20),
+      //           ),
+      //           Padding(padding: EdgeInsets.fromLTRB(100, 20, 100, 0)),
+      //           Container(
+      //           height: 150.0,
+      //           width: 400.0,
+      //           color: Colors.transparent,
+      //           child: Container(
+      //             decoration: BoxDecoration(
+      //                 color: Colors.grey,
+      //                 borderRadius: BorderRadius.all(Radius.circular(5.0))),
+      //         ),
+      //       ),
+      //     ],
+      //   ),
+      // ),
     );
   }
 }
