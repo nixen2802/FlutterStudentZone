6cebdcce24ff452a3d86e1de5bd01160e32baec4
Rushabh Gandhi <rushabh.dg@somaiya.edu>
2021-11-13 18:02:37 +0530

    added detail page

--- lib/screens/productdetail.dart
+++ lib/screens/productdetail.dart
@@ -4,11 +4,7 @@ import 'package:studentzone/screens/base.dart';
 class productdetail extends StatefulWidget {
   static const routeName = '/productdetail';
 
-  const productdetail(
-      {Key? key,
-      
-      required this.doc})
-      : super(key: key);
+  const productdetail({Key? key, required this.doc}) : super(key: key);
 
   // Declare a field that holds the Todo.
   final doc;
@@ -38,7 +34,7 @@ class _productdetailState extends State<productdetail> {
               child: Container(
                 margin: EdgeInsets.all(12.0),
                 width: MediaQuery.of(context).size.width - 50,
-                height: 200,
+                height: 250,
                 decoration: BoxDecoration(
                     image: DecorationImage(
                       fit: BoxFit.fill,
@@ -57,12 +53,71 @@ class _productdetailState extends State<productdetail> {
               ),
             ),
             Container(
-              padding: EdgeInsets.only(top: 20),
+              padding: EdgeInsets.all(20),
               child: Column(
                 children: [
-                  Text("Product Name : " + widget.doc['Name']
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Rs." + widget.doc['price'],
+                      style: TextStyle(fontSize: 28),
+                    ),
+                    // Text( widget.doc['Name'],
+                    // style: TextStyle(fontSize: 28),
+                    // ),
                   ),
-                  Text("Product Price : " + widget.doc['price']),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      widget.doc['Name'],
+                      style: TextStyle(fontSize: 25),
+                    ),
+                  ),
+                  Divider(
+                    height: 1,
+                    thickness: 1,
+                    indent: 20,
+                    endIndent: 20,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Description:",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      widget.doc['description'],
+                      style: TextStyle(fontSize: 17),
+                    ),
+                  ),
+                  Divider(
+                    height: 1,
+                    thickness: 1,
+                    indent: 20,
+                    endIndent: 20,
+                  ),
+                  Row(children: [
+                    CircleAvatar(
+                        radius: 50,
+                        backgroundImage: NetworkImage(
+                            'https://previews.123rf.com/images/jemastock/jemastock1706/jemastock170608711/80128439-young-and-successful-business-man-cartoon-employee-work.jpg')),
+                    Column(        
+                    children:[Text("Rushabh",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20)),
+                    // Padding(padding: EdgeInsets.fromLTRB(100, 20, 100, 0)),
+                    Text("rushabh@gmail.com",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 15)),
+                    // Padding(padding: EdgeInsets.fromLTRB(100, 20, 100, 0)),
+                    ])
+                  ])
                 ],
               ),
             )
